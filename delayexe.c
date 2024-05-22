ea863aa8dee9fc85c5f6285520e6ad1a554be50f
Jay Krell <jay.krell@cornell.edu>
2024-05-21 19:26:52 -0700

    wip

--- delayexe.c
+++ delayexe.c
@@ -4,19 +4,34 @@
 #include "windows.h"
 #include <assert.h>
 
+DWORD threadId[3];
 HANDLE event, thread1, thread2;
 void* initial_imp_delay000;
 
-void ExceptionHandler()
-// Step until __imp_delay000 changes. Then let thread2 resume and run forever.
-// With the bug, thread2 should access violate.
+void LocalSleep()
 {
+    Sleep(-1);
+}
+
+long ExceptionHandler(PEXCEPTION_POINTERS exception)
+// Step thread1 until __imp_delay000 changes. Then let thread2 resume and run forever.
+// With the bug, thread2 should access violate.
+// 
+{
+    printf("ExceptionHandler: %d %p %X\n", GetCurrentThreadId(), (void*)exception->ContextRecord->Rip, *(unsigned char*)exception->ContextRecord->Rip);
+    if (GetCurrentThreadId() != threadId[1])
+    {
+        //return EXCEPTION_CONTINUE_SEARCH;
+    }
     if (initial_imp_delay000 != __imp_delay000)
     {
         SetEvent(event);
-        Sleep(-1);
-        return;
+        //exception->ContextRecord->Rip = (size_t)&LocalSleep;
+        //return EXCEPTION_CONTINUE_EXECUTION;
     }
+    //exception->ContextRecord->EFlags = (size_t)&LocalSleep;
+    ++(exception->ContextRecord->Rip);
+    return EXCEPTION_CONTINUE_EXECUTION;
 }
 
 DWORD Thread1(PVOID p)
@@ -50,9 +65,18 @@ int main()
 
     initial_imp_delay000 = __imp_delay000;
 
+    AddVectoredExceptionHandler(0, ExceptionHandler);
     event = CreateEventW(0, 0, 0, 0);
-    thread1 = CreateThread(0, 0, &Thread1, 0, 0, 0);
-    thread2 = CreateThread(0, 0, &Thread2, 0, 0, 0);
+    threadId[0] = GetCurrentThreadId();
+    thread1 = CreateThread(0, 0, &Thread1, 0, CREATE_SUSPENDED, &threadId[1]);
+    thread2 = CreateThread(0, 0, &Thread2, 0, CREATE_SUSPENDED, &threadId[2]);
+    ResumeThread(thread1);
+    ResumeThread(thread2);
+
+    __debugbreak();
+    __debugbreak();
+    __debugbreak();
+    SetEvent(event);
 
     return 0;
 }
